b42a95cd3a4b240a1a05aba28c5d9fa9c07c830b
Wayne Zhu <wayne.zhu@digitas.com>
2020-04-26 23:54:51 -0400

    Update getAnswer.groovy

--- vars/getAnswer.groovy
+++ vars/getAnswer.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
-def pipelineName = "Returned value from SharedLibrary"
+
 
 def call() {
+    def pipelineName = "Returned value from SharedLibrary"
     echo "From the Shared library : ${env.JOB_NAME}" 
     echo "Defined var from outside of CALL() ${pipelineName}"
-}
+}                                              
