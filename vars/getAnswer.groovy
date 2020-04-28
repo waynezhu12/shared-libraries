d72ae21b685dbf9aca31a94d3c8ff211ffd46181
Wayne Zhu <wayne.zhu@digitas.com>
2020-04-28 09:49:36 -0400

    Update getAnswer.groovy

--- vars/getAnswer.groovy
+++ vars/getAnswer.groovy
@@ -4,7 +4,10 @@
 
 def call() {
     def pipelineName = "Returned value from SharedLibrary"
+
     echo "From the Shared library : ${env.JOB_NAME}" 
     echo "Defined var from outside of CALL() ${pipelineName}"
-    sh "printenv"
+    //sh "printenv"
+    def scannerHome = tool 'SonarQube-Scanner (3.0.3.778)'
+    echo "Sonar Qube Scanner Path: ${scannerHome}" 
 }                                              
