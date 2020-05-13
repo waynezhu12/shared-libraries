082b8c921d108afc0d45996b3a07a4ed5b20874b
Wayne Zhu <wayne.zhu@digitas.com>
2020-05-13 10:16:08 -0400

    Update getAnswer.groovy

--- vars/getAnswer.groovy
+++ vars/getAnswer.groovy
@@ -11,8 +11,8 @@ def call() {
     def scannerHome = tool 'SonarQube-Scanner (3.0.3.778)'
     echo "Sonar Qube Scanner Path: ${scannerHome}" 
     echo "${currentBuild.currentResult}"
-    def azureRegistry ="azure.groupeconnect.io"
-    def hash = sh(script: "git rev-parse HEAD",returnStdout: true).trim()
+    //def azureRegistry ="azure.groupeconnect.io"
+    //def hash = sh(script: "git rev-parse HEAD",returnStdout: true).trim()
     echo "Whatever this is ${hash}"
     //return this
 }                                              
