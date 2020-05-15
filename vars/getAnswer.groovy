362349d767b6de12326eba07f9c28049183d20b1
Wayne Zhu <wayne.zhu@digitas.com>
2020-05-15 17:44:48 -0400

    Update getAnswer.groovy

--- vars/getAnswer.groovy
+++ vars/getAnswer.groovy
@@ -3,14 +3,26 @@
 
 
 def call() {
-    def pipelineName = "Returned value from SharedLibrary"
+    
 
     echo "From the Shared library : ${env.JOB_NAME}" 
-    echo "Defined var from outside of CALL() ${pipelineName}"
+    def jobName = "${env.JOB_NAME}"
+    def supressionFile = "suppression.xml"
+
+ if {JOB_NAME.contains ('lib-S')
+ echo "I got the tight name"
+ }
+ else {
+     echo "${surpressionFile}"
+ }
+
+
+    
     //sh "printenv"
     def scannerHome = tool 'SonarQube-Scanner (3.0.3.778)'
     echo "Sonar Qube Scanner Path: ${scannerHome}" 
     echo "${currentBuild.currentResult}"
+
     //def azureRegistry ="azure.groupeconnect.io"
     //def hash = sh(script: "git rev-parse HEAD",returnStdout: true).trim()
     //echo "Whatever this is ${hash}"
